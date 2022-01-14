d7966a75b1735ab6710652758d0ada99396cdb63
Chanikarn <chanikarn.nn6@gmail.com>
2022-01-14 22:02:04 +0700

    11

---
+++ js.js
@@ -0,0 +1,23 @@
+var From = new VTTCue({
+    el:"fm",
+    data:{
+        student:{
+            Id:"",
+            Name:"",
+            Lastname:"",
+            Email:"",
+            GPA:"",
+
+        },
+        studentList:[
+            {Id:1,Name:"aaa",Lastname:"aaa",Email:"aaa@gmail.com",GPA:"4.00"},
+            {Id:2,Name:"bbb",Lastname:"bbb",Email:"bbb@gmail.com",GPA:"4.00"},
+            {Id:3,Name:"ccc",Lastname:"ccc",Email:"ccc@gmail.com",GPA:"4.00"},
+        ]
+    },
+    methods:{
+        push:function(){
+
+        }
+    }
+})
